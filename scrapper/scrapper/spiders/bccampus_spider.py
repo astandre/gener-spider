ec8014c91edcbbfbf6e4769cfddbea4e5e39d6a8
Andre <andreherrera97@hotmail.com>
2019-08-12 00:22:21 -0500

    Adding updated spiders

--- scrapper/scrapper/spiders/bccampus_spider.py
+++ scrapper/scrapper/spiders/bccampus_spider.py
@@ -6,14 +6,8 @@ from ..services import *
 
 class GenericSpider(scrapy.Spider):
     name = "bccampus"
-    # rules = [['//div[@class="row laText"]/div/h3/a/text()', "hasName"],
-    #          ['//div[@class="row laText"]/div/div/div/a[1]/text()', "hasCreator"]]
 
-    rules = None
-    subject = None
-    next = None
-    urls = None
-    carry_rule = None
+    subject = "open bccampus"
     cont = 0
 
     def start_requests(self):
@@ -21,13 +15,6 @@ class GenericSpider(scrapy.Spider):
             "https://open.bccampus.ca/browse-our-collection/find-open-textbooks/"
         ]
 
-        # data = init_spider("12345")
-        # self.urls = data["urls"]
-        self.subject = "open bccampus"
-        # self.rules = data["rules"]
-        # self.next = data["next"]
-
-        # print(data)
         for url in urls:
             print("New Url", url)
             yield scrapy.Request(url=url, callback=self.parse)
@@ -40,42 +27,12 @@ class GenericSpider(scrapy.Spider):
             # self.carry_rule = None
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        # filename = 'quotes-%s.html' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
-
-        # print(response.xpath('//b/@itemprop="name"'))
-        # print(response.xpath('//b/@itemprop="name"').get())
-        # for rule in self.rules:
-        # contenidos = response.xpath('//div[@class="row laText"]').getall()
-        # print("COntenidos")
-        # values = []
 
         resp = response.xpath("//h4/a/@href").getall()
         if resp is not None:
             for next_page in resp:
                 if next_page is not None:
                     yield response.follow(next_page, self.parse_follow)
-            # yield TripleItem(subject=self.subject, predicate=rule["name"], object=resp_aux)
-
-        # elif rule["type"] == "O":
-        #     resp = response.xpath(rule["rule"]).get()
-        #     if resp is not None:
-        #         yield TripleItem(subject=self.subject, predicate=rule["name"], object=resp)
-        #
-        # elif rule["type"] == "X":
-        #     # print(response.url)
-        #     # print(rule["next"])
-        #     self.carry_rule = rule["next"]
-        #     next_page = response.xpath(rule["rule"]).getall()
-        #     if next_page is not None:
-        #         for page in next_page:
-        #             print("Access  >>", page)
-        #             yield response.follow(page, self.parse_follow)
-        # yield scrapy.Request(url=response.url, callback=self.parse)
-        # print(self.cont)
         pages = response.xpath("//section[@class='p-3']/p/a/@href").getall()
         if self.cont < len(pages):
             next_page = pages[self.cont]
---
+++ scrapper/scrapper/spiders/opentextbooks_spider.py
@@ -0,0 +1,65 @@
+import scrapy
+from scrapy.selector import Selector
+from ..items import *
+from ..services import *
+
+
+class GenericSpider(scrapy.Spider):
+    name = "opentextbooks"
+
+    subject = "Open textBooks"
+
+    def start_requests(self):
+        urls = [
+            "https://open.umn.edu/opentextbooks/subjects/accounting-finance"
+            "https://open.umn.edu/opentextbooks/subjects/business",
+            "https://open.umn.edu/opentextbooks/subjects/human-resources",
+            "https://open.umn.edu/opentextbooks/subjects/management",
+            "https://open.umn.edu/opentextbooks/subjects/marketing",
+            "https://open.umn.edu/opentextbooks/subjects/computer-science-information-systems",
+            "https://open.umn.edu/opentextbooks/subjects/economics",
+            "https://open.umn.edu/opentextbooks/subjects/education",
+            "https://open.umn.edu/opentextbooks/subjects/humanities"
+            "https://open.umn.edu/opentextbooks/subjects/engineering",
+            "https://open.umn.edu/opentextbooks/subjects/arts",
+            "https://open.umn.edu/opentextbooks/subjects/history",
+            "https://open.umn.edu/opentextbooks/subjects/languages",
+            "https://open.umn.edu/opentextbooks/subjects/linguistics"
+            "https://open.umn.edu/opentextbooks/subjects/literature-rhetoric-and-poetry",
+            "https://open.umn.edu/opentextbooks/subjects/philosophy",
+            "https://open.umn.edu/opentextbooks/subjects/journalism-media-studies-communications",
+            "https://open.umn.edu/opentextbooks/subjects/law",
+            "https://open.umn.edu/opentextbooks/subjects/mathematics",
+            "https://open.umn.edu/opentextbooks/subjects/applied",
+            "https://open.umn.edu/opentextbooks/subjects/pure",
+            "https://open.umn.edu/opentextbooks/subjects/medicine",
+            "https://open.umn.edu/opentextbooks/subjects/natural-sciences",
+            "https://open.umn.edu/opentextbooks/subjects/biology",
+            "https://open.umn.edu/opentextbooks/subjects/chemistry",
+            "https://open.umn.edu/opentextbooks/subjects/physics",
+            "https://open.umn.edu/opentextbooks/subjects/social-sciences",
+            "https://open.umn.edu/opentextbooks/subjects/psychology",
+            "https://open.umn.edu/opentextbooks/subjects/sociology",
+            "https://open.umn.edu/opentextbooks/subjects/student-success"
+        ]
+
+        for url in urls:
+            print("New Url", url)
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse_follow(self, response):
+
+        resp = response.xpath("//div[@id='maincontent']").get()
+        if resp is not None:
+            yield TripleItem(subject=self.subject, predicate="hasRaw", object=resp)
+
+    def parse(self, response):
+        resp = response.xpath("//a[contains(text(),'READ MORE')]/@href").getall()
+        if resp is not None:
+            for next_page in resp:
+                if next_page is not None:
+                    yield response.follow(next_page, self.parse_follow)
+        next_page = response.xpath("//a[@rel='next'][1]/@href").get()
+        print("Next Page >>", next_page)
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
